23dca50fb19f9c4c45e4d4cd82eda4015713bce3
23WH1A0544 <23wh1a0544@bvrithyderabad.edu.in>
2026-04-10 21:53:38 +0530

    corrected

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path ="/" element ={=<TaskList/>}/>
+        <Route path ="/" element ={<TaskList/>}/>
         <Route path = "/add-task" element ={<AddTask/>}/>
       </Routes>
     </BrowserRouter>
