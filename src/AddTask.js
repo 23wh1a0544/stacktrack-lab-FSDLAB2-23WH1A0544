1dc7a9d37796a55455a9647fd2b5ae6adfbd3dee
23WH1A0544 <23wh1a0544@bvrithyderabad.edu.in>
2026-04-10 21:45:12 +0530

    integrated API

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,26 +1,53 @@
-import React,{useState} from "react";
-function AddTask(){
-    const [title,setTitle] = useState("");
-    const [description,setDescription]= useState("");
-    return (
-        <div>
-            <h2>Add Task</h2>
-            <input 
-                type = "text"
-                placeholder="Enter Title"
-                value = {title}
-                onChange={(e)=>setTitle(e.target.value)}/>
-                <br /><br />
+import React, { useState } from "react";
 
-                <input
-                    type ="text"
-                    placeholder="Enter Description"
-                    value = {description}
-                    onChange={(e)=>setDescription(e.target.value)}/>
-                    <br /><br />
-                    <button></button>
+function AddTask() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-        </div>
-    );
+  const handleSubmit = () => {
+    const taskData = {
+      title: title,
+      description: description,
+      status: "pending"
+    };
+
+    fetch("http://bvrithcloud.com/api/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-student-id": "23WH1A05xx"
+      },
+      body: JSON.stringify(taskData)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Add Task</h2>
+
+      <input
+        type="text"
+        placeholder="Enter Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <br /><br />
+
+      <input
+        type="text"
+        placeholder="Enter Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <br /><br />
+
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
+
 export default AddTask;
